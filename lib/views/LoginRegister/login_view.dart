5ed73cbfec29b7123248c6a28d321b0e3fe88937
Fabien Fiette <fabienfiette@hotmail.fr>
2023-11-09 14:43:48 +0100

    feat: scroll view when focus on textfield

--- lib/views/LoginRegister/login_view.dart
+++ lib/views/LoginRegister/login_view.dart
@@ -71,95 +71,95 @@ class LoginBottomSheetState extends State<LoginBottomSheet> {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => FocusScope.of(context).unfocus(),
-      child: Container(
-        padding: const EdgeInsets.fromLTRB(20, 8, 20, 60),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              // Barre visuel pour le BottomSheet
-              Container(
-                width: 160,
-                height: 8,
-                decoration: BoxDecoration(
-                  color: MyColors.neutral70,
-                  borderRadius: BorderRadius.circular(8),
+    return Container(
+      padding: const EdgeInsets.fromLTRB(20, 8, 20, 60),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            // Barre visuel pour le BottomSheet
+            Container(
+              width: 160,
+              height: 8,
+              decoration: BoxDecoration(
+                color: MyColors.neutral70,
+                borderRadius: BorderRadius.circular(8),
+              ),
+            ),
+            const SizedBox(height: 20),
+            if (errorMessage.isNotEmpty)
+              Padding(
+                padding: const EdgeInsets.only(bottom: 20),
+                child: Text(
+                  errorMessage,
+                  style: BodyLargeMedium.copyWith(color: MyColors.error40),
                 ),
               ),
-              const SizedBox(height: 20),
-              if (errorMessage.isNotEmpty)
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 20),
-                  child: Text(
-                    errorMessage,
-                    style: BodyLargeMedium.copyWith(color: MyColors.error40),
-                  ),
-                ),
-              CustomTextField(
-                placeholder: 'E-mail',
-                controller: _emailController,
-                validator: (value) {
-                  if (value == null || value.isEmpty || !RegExp(r'\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b').hasMatch(value)) {
-                    return "L'adresse e-mail n'est pas valide.";
-                  }
-                  return null;
-                },
-                keyboardType: TextInputType.emailAddress,
-              ),
-              const SizedBox(height: 24),
-              CustomTextField(
-                placeholder: 'Mot de passe',
-                controller: _passwordController,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Le mot de passe ne peut pas être vide.';
-                  }
-                  return null;
-                },
-                obscureText: !visiblePassword,
-                rightIcon: visiblePassword
-                    ? Icons.visibility_off_outlined
-                    : Icons.visibility_outlined,
-                onRightIconPressed: () =>
-                    setState(() => visiblePassword = !visiblePassword),
-              ),
-              const SizedBox(height: 8),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: InkWell(
-                  onTap: () {},
-                  child: Text(
-                    'Mot de passe oublié ?',
-                    style: BodyLargeMedium.copyWith(color: MyColors.secondary40),
-                  ),
+            CustomTextField(
+              placeholder: 'E-mail',
+              controller: _emailController,
+              validator: (value) {
+                if (value == null ||
+                    value.isEmpty ||
+                    !RegExp(r'\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b')
+                        .hasMatch(value)) {
+                  return "L'adresse e-mail n'est pas valide.";
+                }
+                return null;
+              },
+              keyboardType: TextInputType.emailAddress,
+            ),
+            const SizedBox(height: 24),
+            CustomTextField(
+              placeholder: 'Mot de passe',
+              controller: _passwordController,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Le mot de passe ne peut pas être vide.';
+                }
+                return null;
+              },
+              obscureText: !visiblePassword,
+              rightIcon: visiblePassword
+                  ? Icons.visibility_off_outlined
+                  : Icons.visibility_outlined,
+              onRightIconPressed: () =>
+                  setState(() => visiblePassword = !visiblePassword),
+            ),
+            const SizedBox(height: 8),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: InkWell(
+                onTap: () {},
+                child: Text(
+                  'Mot de passe oublié ?',
+                  style: BodyLargeMedium.copyWith(color: MyColors.secondary40),
                 ),
               ),
-              const SizedBox(height: 60),
-              CustomButton(
-                label: 'Continuer',
-                onPressed: () => signIn(),
-              ),
-              const SizedBox(height: 20),
-              InkWell(
-                onTap: () => Navigator.pushNamed(context, '/register'),
-                child: RichText(
-                  text: TextSpan(
-                    style: BodyLargeMedium,
-                    children: const <TextSpan>[
-                      TextSpan(text: 'Pas encore membre ? '),
-                      TextSpan(
-                        text: 'Rejoignez-nous !',
-                        style: TextStyle(color: MyColors.secondary40),
-                      ),
-                    ],
-                  ),
+            ),
+            const SizedBox(height: 60),
+            CustomButton(
+              label: 'Continuer',
+              onPressed: () => signIn(),
+            ),
+            const SizedBox(height: 20),
+            InkWell(
+              onTap: () => Navigator.pushNamed(context, '/register'),
+              child: RichText(
+                text: TextSpan(
+                  style: BodyLargeMedium,
+                  children: const <TextSpan>[
+                    TextSpan(text: 'Pas encore membre ? '),
+                    TextSpan(
+                      text: 'Rejoignez-nous !',
+                      style: TextStyle(color: MyColors.secondary40),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
