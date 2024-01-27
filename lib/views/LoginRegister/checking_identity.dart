134d8f9ea84a3a58ffda9d7cf023c9d953a7606a
JulienBois <71647803+JulienBois@users.noreply.github.com>
2024-01-27 20:14:04 +0100

    Updated the Identity checking view

--- lib/views/LoginRegister/checking_identity.dart
+++ lib/views/LoginRegister/checking_identity.dart
@@ -1,15 +1,55 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:swafe/components/Button/button.dart';
 
 class CheckingIdentity extends StatelessWidget {
   const CheckingIdentity({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(children:
-      [
-        Text("Vérification d'identité")
-      ]),
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(top: 60),
+        child: Column(
+          children: [
+            Text("Vérification d'identité"),
+            Divider(thickness: 1.5),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              child: Column(
+                children: [
+                  Icon(Icons.access_time_outlined, size: 75),
+                  Text(
+                    "La vérification de votre pièce d'identité est en cours",
+                    textAlign: TextAlign.start,
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                      "Merci d'avoir effectué cette étape importante. Si nous avons besoin de plus d'informations, nous vous le ferons savoir dès que possible."),
+                  const SizedBox(height: 20),
+                  Text(
+                      "En attendant, vous pouvez reprendre là où vous en étiez."),
+                ],
+              ),
+            ),
+            const Spacer(),
+            Divider(thickness: 1.5),
+            const SizedBox(height: 10),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: CustomButton(
+                  label: "Terminé",
+                  onPressed: () => Navigator.pushNamed(context, "/home"),
+                  type: ButtonType.outlined,
+                  fillColor: Colors.black,
+                  strokeColor: Colors.black,
+                  textColor: Colors.white),
+            ),
+            const SizedBox(height: 40),
+          ],
+        ),
+      ),
     );
   }
 }
