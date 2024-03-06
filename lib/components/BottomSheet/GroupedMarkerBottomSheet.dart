3e291493464d64d56eb7d88e25722925d5981100
JulienBois <71647803+JulienBois@users.noreply.github.com>
2024-03-06 21:45:20 +0100

    Added modal to show list of reports

---
+++ lib/components/BottomSheet/GroupedMarkerBottomSheet.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+import '../../DS/reporting_type.dart';
+import '../../DS/shadows.dart';
+import '../../DS/spacing.dart';
+import '../../DS/typographies.dart';
+import '../../models/signalement/signalement_model.dart';
+
+class GroupedMarkerBottomSheet extends StatelessWidget {
+  final List<SignalementModel> reports;
+  const GroupedMarkerBottomSheet({super.key, required this.reports});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+          padding: const EdgeInsets.fromLTRB(20, 8, 20, 20),
+          height: 300,
+          child: Column(
+            children: [
+              const SizedBox(
+                height: Spacing.standard,
+              ),
+              Text(
+                'Signalements',
+                style: TitleLargeMedium,
+              ),
+              const SizedBox(
+                height: Spacing.standard,
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: reports.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Column(children:[ ListTile(
+                      leading: Container(
+                        width: 50,
+                        height: 50,
+                        decoration: ShapeDecoration(
+                            image: DecorationImage(
+                              image: AssetImage(convertStringToReportingType(reports[index].selectedDangerItems.first).pin),
+                              fit: BoxFit.fill,
+                            ),
+                            shape: const OvalBorder(
+                              side: BorderSide(width: 1, color: Colors.white),
+                            ),
+                            shadows: Shadows.shadow1),
+                      ),
+                      title: Text(convertStringToReportingType(reports[index].selectedDangerItems.first).title),
+                    ),
+                      const SizedBox(
+                        height: Spacing.standard,
+                      )]);
+                  },
+                ),
+              ),
+            ],
+          ),
+        );
+  }
+}
